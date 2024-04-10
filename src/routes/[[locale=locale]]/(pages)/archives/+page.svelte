53a784e71121d5d375c10e2e0b6647d02c6ba625
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2024-04-09 21:57:08 -0700

    add: archives

---
+++ src/routes/[[locale=locale]]/(pages)/archives/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { t } from "$lib/translations";
+    import PostTimeline from "$lib/components/post/timeline.svelte";
+    import { Title, DescriptionMeta } from "$lib/components/seo";
+
+    /** @type {import('./$types').PageData} */
+    export let data: any;
+
+    $: ({ posts, siteConfig } = data);
+</script>
+
+<Title value={$t("common.archives")}></Title>
+<DescriptionMeta value={$t("common.archives")}></DescriptionMeta>
+
+<svelte:head>
+    {#if siteConfig.keywords}
+        <meta name="keywords" content={siteConfig.keywords.join(",")} />
+    {/if}
+    {#if siteConfig.url}
+        <link rel="canonical" href="{siteConfig.url}/archives/" />
+        <meta property="og:url" content="{siteConfig.url}/archives/" />
+    {/if}
+</svelte:head>
+
+<div class="container archives">
+    <h1>{$t("common.archives")}</h1>
+
+    <PostTimeline {posts} />
+</div>
+
+<style>
+    .archives {
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+    }
+</style>
