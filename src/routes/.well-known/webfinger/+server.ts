cbecf20075afd0ccdaa78784601f5451c152c6c1
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2024-04-11 02:31:33 -0700

    update: webfinger

--- src/routes/.well-known/webfinger/+server.ts
+++ src/routes/.well-known/webfinger/+server.ts
@@ -1,11 +1,21 @@
-import { getSiteConfig } from "$lib/server/config";
+import { getSiteConfig, getSystemConfig } from "$lib/server/config";
 
 export async function GET({ locals }) {
     const { site } = locals;
 
     const siteConfig = getSiteConfig(site, 'en');
+    const systemConfig = getSystemConfig(site);
 
-    let responseText = JSON.stringify({});
+    let responseText = JSON.stringify({
+        "subject": "acct:youruser@your.instance.url",
+        "links": [
+            {
+                "rel": "self",
+                "type": "application/activity+json",
+                "href": "https://your.instance.url/users/youruser"
+            }
+        ]
+    });
 
     return new Response(responseText,
         {
