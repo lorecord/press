a098cec242d6a1d497b0d1d12a8fe355ac7e41a6
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2024-05-30 07:53:36 -0700

    update

--- src/routes/[[locale=locale]]/(pages)/[...route=route]/+page.ts
+++ src/routes/[[locale=locale]]/(pages)/[...route=route]/+page.ts
@@ -17,19 +17,17 @@ export async function load({ params, fetch, parent }) {
     const newer = writable();
     const earlier = writable();
 
-    const post = await fetch(`/api/v1/post/${route}${lang ? '?' + new URLSearchParams({
+    const { status, json: post }: { status: number, json: any } = await fetch(`/api/v1/post/${route}${lang ? '?' + new URLSearchParams({
         lang: get(derivedLang)
     }) : ''}`).then((r) => {
-        if (r.ok) {
-            return r.json()
-        }
+        return { status: r.status, json: r.ok ? r.json() : {} };
     });
 
     if (!post?.published) {
-        error(409); // Gone
+        error(status);
     }
 
-    const { replies, mentions } = post?.comment?.enable ? await fetch(`/api/v1/interaction/${route}`).then((r) => r.json()) : {replies: [], mentions: []};
+    const { replies, mentions } = post?.comment?.enable ? await fetch(`/api/v1/interaction/${route}`).then((r) => r.json()) : { replies: [], mentions: [] };
 
     post.newer && await fetch(`/api/v1/post/${post.newer}${get(derivedLang) ? '?' + new URLSearchParams({
         lang: get(derivedLang)
