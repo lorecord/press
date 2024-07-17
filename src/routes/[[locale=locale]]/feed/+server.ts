69d6c8013fcb1e7672a203b07c8fa4df527b274e
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2024-07-17 09:47:53 +0300

    update

--- src/routes/[[locale=locale]]/feed/+server.ts
+++ src/routes/[[locale=locale]]/feed/+server.ts
@@ -51,11 +51,11 @@ export async function GET({ request, locals }) {
 
 function escapeHtml(unsafe: string) {
     return unsafe
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
+        // .replace(/&/g, "&amp;")
+        // .replace(/</g, "&lt;")
+        // .replace(/>/g, "&gt;")
+        // .replace(/"/g, "&quot;")
+        // .replace(/'/g, "&#039;");
 }
 
 /**
