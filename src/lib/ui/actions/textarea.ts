fc9f12d9d069d0367d36fc3e4d7a08378cb53d77
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2024-08-09 22:22:37 +0530

    update

--- src/lib/ui/actions/textarea.ts
+++ src/lib/ui/actions/textarea.ts
@@ -1,12 +1,15 @@
 export function autogrow(textarea: HTMLTextAreaElement) {
+  let originalStyleResize = textarea.style.resize;
+  let originalHeight = textarea.style.height;
+  textarea.style.resize = 'none';
+
   function adjustHeight() {
     textarea.style.height = 'auto';
     textarea.style.height = textarea.scrollHeight + 'px';
-    textarea.style.resize = 'none';
   }
 
   textarea.addEventListener('input', adjustHeight);
-  // it will catch when  `textarea.value = 'new value'`
+  // it will catch when `textarea.value = 'new value'`
   textarea.addEventListener('change', adjustHeight);
 
   adjustHeight();
@@ -14,7 +17,9 @@ export function autogrow(textarea: HTMLTextAreaElement) {
   return {
     destroy() {
       textarea.removeEventListener('input', adjustHeight);
-      textarea.addEventListener('change', adjustHeight);
+      textarea.removeEventListener('change', adjustHeight);
+      textarea.style.resize = originalStyleResize;
+      textarea.style.height = originalHeight;
     }
   };
 }
