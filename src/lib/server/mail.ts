ab2cda0e9c5c2a563c5d6445381813bbd0fe6b70
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2024-08-13 16:42:45 +0900

    fix

--- src/lib/server/mail.ts
+++ src/lib/server/mail.ts
@@ -93,7 +93,7 @@ export const sendNewReplyMail = async (site: any, post: any, comment: Reply, rep
     let params: any = {
         site_title: siteConfig.title,
         post_title: post.title,
-        replied_author: '> ' + (replied.author?.name || (replied.author?.email?.value ? get(l)(lang, `common.comment_nobody`) : get(l)(lang, `common.comment_anonymous`))),
+        replied_author: (replied.author?.name || (replied.author?.email?.value ? get(l)(lang, `common.comment_nobody`) : get(l)(lang, `common.comment_anonymous`))),
         replied_date: new Intl.DateTimeFormat(lang, {
             dateStyle: "short",
             timeStyle: "short",
