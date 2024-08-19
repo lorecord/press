6e8e98d0fc1e4457e564c3807e94c5af3c5b9f82
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2024-08-19 18:05:12 +0900

    update: links

--- src/lib/markdown/remark-links.ts
+++ src/lib/markdown/remark-links.ts
@@ -1,10 +1,14 @@
-import { visit } from 'unist-util-visit';
-import type { Plugin } from 'unified';
 import { parse } from 'node-html-parser';
+import type { Plugin } from 'unified';
+import { visit } from 'unist-util-visit';
 
 // This plugin extracts external links from a markdown tree
 const remarkLinks: Plugin = () => (tree: any, file: any) => {
-    let links: { href: string, type: 'link' | 'reference' | 'citation' | 'reply' | 'translate' | 'bookmark', content?: string }[] = [];
+    let links: {
+        href: string,
+        type: 'mention' | 'reference' | 'citation' | 'reply' | 'translate' | 'bookmark' | 'license' | 'tag' | 'mention' | 'like' | 'repost' | 'rsvp';
+        content?: string,
+    }[] = [];
 
     // `reference` means link to another webpage in footnote
     visit(tree, 'footnoteReference', (node: any) => {
@@ -16,7 +20,7 @@ const remarkLinks: Plugin = () => (tree: any, file: any) => {
     visit(tree, 'link', (node: any) => {
         const href = node.url;
         const title = node.title || '';
-        let type: any = 'link';
+        let type: any = 'mention';
 
         // The text content near to the link, contains text from sublings nodes of current node
         const text = node.sublings?.map((n: any) => n.value).join('');
@@ -36,7 +40,38 @@ const remarkLinks: Plugin = () => (tree: any, file: any) => {
         const as = doc.querySelectorAll('a[href]');
         for (const a of as) {
             const href = a.getAttribute('href') as string;
-            links.push({ href, type: 'link', content: a.textContent as string });
+
+            if (a.classList?.length > 0) {
+                for (let className of a.classList.values()) {
+                    if (className === 'u-in-reply-to') {
+                        links.push({ href, type: 'reply', content: a.textContent as string });
+                    } else if (className === 'u-repost-of') {
+                        links.push({ href, type: 'repost', content: a.textContent as string });
+                    } else if (className === 'u-like-of') {
+                        links.push({ href, type: 'like', content: a.textContent as string });
+                    } else if (className === 'u-bookmark-of') {
+                        links.push({ href, type: 'bookmark', content: a.textContent as string });
+                    } else if (className === 'u-tag-of') {
+                        links.push({ href, type: 'tag', content: a.textContent as string });
+                    } else if (className === 'u-license') {
+                        links.push({ href, type: 'license', content: a.textContent as string });
+                    } else if (className === 'u-mention') {
+                        links.push({ href, type: 'mention', content: a.textContent as string });
+                    } else if (className === 'u-rsvp') {
+                        links.push({ href, type: 'rsvp', content: a.textContent as string });
+                    } else if (className === 'u-translate') {
+                        links.push({ href, type: 'translate', content: a.textContent as string });
+                    } else if (className.startsWith('u-rsvp-')) {
+                        links.push({ href, type: 'rsvp', content: a.textContent as string });
+                    }
+                }
+            } else {
+                if (a.classList?.length) {
+                    console.log('a.classList', a.classList);
+                }
+
+                links.push({ href, type: 'mention', content: a.textContent as string });
+            }
         }
     });
 
@@ -46,7 +81,5 @@ const remarkLinks: Plugin = () => (tree: any, file: any) => {
 
     file.data.links = links;
 };
-
-
 
 export default remarkLinks;
