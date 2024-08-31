eb9b44f20a4b3cf62dde9818f058a838fa56fd3f
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2024-08-31 18:21:05 +0530

    update: feed

--- src/lib/post/handle-feed.ts
+++ src/lib/post/handle-feed.ts
@@ -4,6 +4,236 @@ import { t } from "$lib/translations";
 import { convertToPost } from "./handle-posts";
 import type { Post, PostRaw } from "./types";
 
+export type FeedRender = (posts: Post[], lang: string, siteConfig: any, defaultAuthor: any, supportedLocales: string[], websub: any) => string;
+
+export const renderFeed = (requestAcceptHeader: string | null, url: any, posts: Post[], lang: string, siteConfig: any, defaultAuthor: any, supportedLocales: string[], websub: any): { body: string, headers: { [key: string]: string } } => {
+
+    let type = 'rss';
+
+    let contentType = 'application/rss+xml;charset=UTF-8';
+
+    if ((requestAcceptHeader && requestAcceptHeader.includes('application/atom+xml'))
+        || url.endsWith('atom')) {
+        type = 'atom';
+        contentType = 'application/atom+xml;charset=UTF-8';
+    } else if ((requestAcceptHeader && requestAcceptHeader.includes('application/feed+json'))
+        || url.endsWith('json')) {
+        type = 'json';
+        contentType = 'application/feed+json;charset=UTF-8';
+    } else if ((requestAcceptHeader && requestAcceptHeader.includes('application/json'))
+        || url.endsWith('json')) {
+        type = 'json';
+        contentType = 'application/json;charset=UTF-8';
+    }
+
+    const renderMap: { [key: string]: FeedRender } = {
+        'rss': renderRss,
+        'atom': renderAtom,
+        'json': renderJson
+    }
+
+    const headers: any = {
+        'Content-Type': contentType,
+        'Cache-Control': 'max-age=604800',
+    };
+
+    const body = renderMap[type](posts, lang, siteConfig, defaultAuthor, supportedLocales, websub);
+
+    return { body, headers };
+}
+
+
+function escapeHtml(unsafe: string) {
+    return unsafe
+    // .replace(/&/g, "&amp;")
+    // .replace(/</g, "&lt;")
+    // .replace(/>/g, "&gt;")
+    // .replace(/"/g, "&quot;")
+    // .replace(/'/g, "&#039;");
+}
+
+/**
+ * https://www.jsonfeed.org/version/1.1/
+ */
+const renderJson = (posts: Post[], lang: string, siteConfig: any, defaultAuthor: any, supportedLocales: string[], websubConfig?: { enabled: boolean, endpoint?: string | string[] }) => {
+
+    const feed: any = {
+        version: "https://jsonfeed.org/version/1.1",
+        title: siteConfig.title,
+        home_page_url: siteConfig.url,
+        feed_url: `${siteConfig.url}/feed/`,
+    };
+
+    if (siteConfig.description) {
+        feed.description = siteConfig.description;
+    }
+
+    if (defaultAuthor) {
+        feed.authors = [{
+            name: defaultAuthor.name,
+            url: defaultAuthor.url,
+            avatar: defaultAuthor.avatar
+        }]
+    }
+
+    // next_url
+    // icon
+    // favicon
+    // authors
+    feed.language = lang;
+    // expired
+    if (websubConfig?.enabled) {
+        feed.hubs = [websubConfig.endpoint || 'https://pubsubhubbub.superfeedr.com'].flat().filter(u => !!u);
+    }
+
+    feed.items = posts.map((post) => {
+        const item: any = {
+            id: `${siteConfig.url}${post.route}`,
+            url: `${siteConfig.url}${post.route}`,
+            // external_url: post.external_url,
+            title: post.title,
+            date_published: post.published?.date,
+            // content_text: post.content?.raw,
+            content_html: post.content?.html,
+            summary: post.summary?.raw,
+            authors: post.author?.map((author: any) => ({
+                name: author.name,
+                url: author.url,
+                avatar: author.avatar
+            })),
+            language: post.lang,
+            tags: [...(post.taxonomy?.series || []), ...(post.taxonomy?.category || []), ...(post.taxonomy?.tag || [])],
+        };
+        if (post.image?.[0] || post.photo?.[0]) {
+            item.image = `${siteConfig.url}${post.image?.[0] || post.photo?.[0]}`;
+        }
+        // banner_image = 
+        if (post.modified?.date) {
+            item.date_modified = post.modified?.date;
+        }
+
+        return item;
+    });
+
+    return JSON.stringify(feed);
+}
+
+/**
+ * https://www.rssboard.org/rss-specification
+ * 
+ * 1. pubDate: https://www.w3.org/Protocols/rfc822/
+ * 2. check: https://validator.w3.org/feed/
+ * @param posts 
+ * @param lang 
+ * @returns 
+ */
+const renderRss = (posts: any, lang: string, siteConfig: any, defaultAuthor: any, supportedLocales: string[], websubConfig?: { enabled: boolean, endpoint?: string }) => (`<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" 
+    xmlns:atom="http://www.w3.org/2005/Atom"
+    xmlns:content="http://purl.org/rss/1.0/modules/content/">
+<channel>
+    <atom:link href="${siteConfig.url}/feed/" rel="self" type="application/rss+xml" />
+    <title>${siteConfig.title}</title>
+    <description>${siteConfig.description}</description>
+    <link>${siteConfig.url}</link>
+    ${websubConfig?.enabled
+        ? [websubConfig.endpoint || 'https://pubsubhubbub.superfeedr.com'].flat().filter(u => !!u).map(u => `<link rel="hub" href="${u}" /><atom:link rel="hub" href="${u}" />`) : ``}
+    <language>${lang}</language>
+    <generator>Press</generator>
+    <docs>https://www.rssboard.org/rss-specification</docs>
+${posts.map((post: any) => `
+    <item>
+        <guid isPermaLink="true">${siteConfig.url}${post.route}</guid>
+        <title>${post.title}</title>
+        <link>${siteConfig.url}${post.route}</link>
+        <description><![CDATA[${escapeHtml(post.summary?.html)}]]></description>
+        <content:encoded><![CDATA[${escapeHtml(post.content?.html)}]]></content:encoded>
+        <pubDate>${new Date(post.published.date).toUTCString()}</pubDate>
+        ${post.taxonomy?.category
+                ? post.taxonomy.category
+                    .map((category: any) => `<category>${category}</category>`).join('\n')
+                : ''}
+        ${post.taxonomy?.tag
+                ? post.taxonomy.tag
+                    .map((tag: any) => `<category>${tag}</category>`).join('\n\t\t') : ''}
+    </item>`
+        )
+        .join('')}
+</channel>
+</rss>
+`)
+
+/**
+ * https://datatracker.ietf.org/doc/html/rfc4287
+ * @param posts 
+ * @param lang 
+ * @returns 
+ */
+const renderAtom = (posts: any, lang: string, siteConfig: any, defaultAuthor: any, supportedLocales: string[], websubConfig?: { enabled: boolean, endpoint?: string }) => (`<?xml version="1.0" encoding="UTF-8" ?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <id>${siteConfig.url}</id>
+    <title type="text">${siteConfig.title}</title>
+    <subtitle type="text">${siteConfig.description}</subtitle>
+    <link href="${siteConfig.url}" />
+    ${websubConfig?.enabled
+        ? [websubConfig.endpoint || 'https://pubsubhubbub.superfeedr.com'].flat().filter(u => !!u).map(u => `<link rel="hub" href="${u}" />`) : ``}
+    ${defaultAuthor ? `<author>
+        <name>${defaultAuthor?.name}</name>
+    </author>` : ``
+    }${posts?.[0]?.modified?.date || posts?.[0]?.published?.date ? `<updated>${new Date(posts?.[0]?.modified?.date || posts?.[0]?.published?.date).toISOString()}</updated>` : ``}
+    <generator uri="https://press.lorecord.com" version="0.0.1">Press</generator>
+    <link href="${siteConfig.url}" rel="alternate" type="text/html"/>
+    <link href="${siteConfig.url}/feed/" rel="self" type="application/atom+xml"/>
+${supportedLocales.map((locale: any) => {
+        return `<link href="${siteConfig.url}/${locale}/feed/" rel="alternate" type="application/atom+xml" hreflang="${locale}" />`
+    })}
+    <rights>Copyright (c)</rights>
+${posts.map((post: any) => `
+    <entry>
+        <id isPermaLink="true">${siteConfig.url}${post.route}</id>
+        <title>${post.title}</title>
+        <link href="${siteConfig.url}${post.route}" />
+        ${post.published.date
+            ? `<published>${new Date(post.published.date).toISOString()}</published>`
+            : ``}
+        ${post.modified?.date
+            ? `<updated>${new Date(post.modified?.date).toISOString()}</updated>`
+            : ``}
+        ${post.audio?.[0]
+            ? `<link rel="enclosure" type="audio/mpeg"
+        href="${post.audio?.[0]}"/>`
+            : ``}
+            ${post.video?.[0]
+            ? `<link rel="enclosure" type="video"
+            href="${post.video?.[0]}"/>`
+            : ``}
+        ${post.author
+            ? post.author.map((author: any) => `<author>
+            <name>${author.name}</name>
+        </author>`)
+            : ``}
+        ${post.contributor
+            ? post.contributor.map((c: any) => `
+        <contributor>
+            <name>${c}</name>
+        </contributor>
+            `)
+            : ``}
+        <summary><![CDATA[${escapeHtml(post.summary?.html)}]]></summary>
+        <content type="html"><![CDATA[${escapeHtml(post.content?.html)}]]></content>
+        ${post.taxonomy?.category
+            ? post.taxonomy.category
+                .map((category: any) => `<category>${category}</category>`).join('\n')
+            : ''}
+        ${post.taxonomy?.tag
+            ? post.taxonomy.tag
+                .map((tag: any) => `<category>${tag}</category>`).join('\n\t\t') : ''}
+    </entry>`
+    )
+        .join('')}
+</feed>
+`)
+
 export function convertToPostForFeed(site: Site, raw: PostRaw) {
     let post: Post = convertToPost(site, raw, false);
     const siteConfig = getSiteConfig(site, post.lang);
--- src/routes/[[locale=locale]]/feed/+server.ts
+++ src/routes/[[locale=locale]]/feed/+server.ts
@@ -1,4 +1,4 @@
-import { convertToPostForFeed } from "$lib/post/handle-feed";
+import { convertToPostForFeed, renderFeed } from "$lib/post/handle-feed";
 import { getSiteAccount } from "$lib/server/accounts.js";
 import { getSiteConfig, getSystemConfig } from "$lib/server/config";
 import { getPublicPostRaws } from "$lib/server/posts";
@@ -18,16 +18,6 @@ export async function GET({ request, locals, params, url }) {
 
     const accept = request.headers.get('accept');
 
-    let type = 'rss';
-
-    let contentType = 'application/rss+xml;charset=UTF-8';
-
-    if ((accept && accept.includes('application/atom+xml'))
-        || request.url.endsWith('atom')) {
-        type = 'atom';
-        contentType = 'application/atom+xml;charset=UTF-8';
-    }
-
     let postRaws = getPublicPostRaws(site);
 
     let posts = postRaws.map((p) => convertToPostForFeed(site, p))
@@ -46,14 +36,7 @@ export async function GET({ request, locals, params, url }) {
         ]),
     );
 
-    let body = type === 'atom'
-        ? renderAtom(posts, lang, siteConfig, defaultAuthor, supportedLocales, systemConfig.websub)
-        : renderRss(posts, lang, siteConfig, defaultAuthor, supportedLocales, systemConfig.websub);
-
-    const headers: any = {
-        'Content-Type': contentType,
-        'Cache-Control': 'max-age=604800',
-    };
+    const { body, headers } = renderFeed(accept, url, posts, lang, siteConfig, defaultAuthor, supportedLocales, systemConfig.websub);
 
     if (systemConfig.websub?.enabled) {
         headers['Link'] = `<${url.pathname}>; rel="self", ` + [systemConfig.websub.endpoint || 'https://pubsubhubbub.superfeedr.com'].flat().filter(u => !!u).map(u => `<${u}>; rel="hub"`).join(' ');
@@ -61,135 +44,6 @@ export async function GET({ request, locals, params, url }) {
 
     return new Response(body, {
         status: 200,
-        headers: {
-            'Content-Type': contentType,
-            'Cache-Control': 'max-age=604800',
-
-        }
+        headers
     });
 }
-
-function escapeHtml(unsafe: string) {
-    return unsafe
-    // .replace(/&/g, "&amp;")
-    // .replace(/</g, "&lt;")
-    // .replace(/>/g, "&gt;")
-    // .replace(/"/g, "&quot;")
-    // .replace(/'/g, "&#039;");
-}
-
-/**
- * https://www.rssboard.org/rss-specification
- * 
- * 1. pubDate: https://www.w3.org/Protocols/rfc822/
- * 2. check: https://validator.w3.org/feed/
- * @param posts 
- * @param lang 
- * @returns 
- */
-const renderRss = (posts: any, lang: string, siteConfig: any, defaultAuthor: any, supportedLocales: string[], websubConfig?: { enabled: boolean, endpoint?: string }) => (`<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0" 
-    xmlns:atom="http://www.w3.org/2005/Atom"
-    xmlns:content="http://purl.org/rss/1.0/modules/content/">
-<channel>
-    <atom:link href="${siteConfig.url}/feed/" rel="self" type="application/rss+xml" />
-    <title>${siteConfig.title}</title>
-    <description>${siteConfig.description}</description>
-    <link>${siteConfig.url}</link>
-    ${websubConfig?.enabled
-        ? [websubConfig.endpoint || 'https://pubsubhubbub.superfeedr.com'].flat().filter(u => !!u).map(u => `<link rel="hub" href="${u}" /><atom:link rel="hub" href="${u}" />`) : ``}
-    <language>${lang}</language>
-    <generator>Press</generator>
-    <docs>https://www.rssboard.org/rss-specification</docs>
-${posts.map((post: any) => `
-    <item>
-        <guid isPermaLink="true">${siteConfig.url}${post.route}</guid>
-        <title>${post.title}</title>
-        <link>${siteConfig.url}${post.route}</link>
-        <description><![CDATA[${escapeHtml(post.summary?.html)}]]></description>
-        <content:encoded><![CDATA[${escapeHtml(post.content?.html)}]]></content:encoded>
-        <pubDate>${new Date(post.published.date).toUTCString()}</pubDate>
-        ${post.taxonomy?.category
-                ? post.taxonomy.category
-                    .map((category: any) => `<category>${category}</category>`).join('\n')
-                : ''}
-        ${post.taxonomy?.tag
-                ? post.taxonomy.tag
-                    .map((tag: any) => `<category>${tag}</category>`).join('\n\t\t') : ''}
-    </item>`
-        )
-        .join('')}
-</channel>
-</rss>
-`)
-
-/**
- * https://datatracker.ietf.org/doc/html/rfc4287
- * @param posts 
- * @param lang 
- * @returns 
- */
-const renderAtom = (posts: any, lang: string, siteConfig: any, defaultAuthor: any, supportedLocales: string[], websubConfig?: { enabled: boolean, endpoint?: string }) => (`<?xml version="1.0" encoding="UTF-8" ?>
-<feed xmlns="http://www.w3.org/2005/Atom">
-    <id>${siteConfig.url}</id>
-    <title type="text">${siteConfig.title}</title>
-    <subtitle type="text">${siteConfig.description}</subtitle>
-    <link href="${siteConfig.url}" />
-    ${websubConfig?.enabled
-        ? [websubConfig.endpoint || 'https://pubsubhubbub.superfeedr.com'].flat().filter(u => !!u).map(u => `<link rel="hub" href="${u}" />`) : ``}
-    ${defaultAuthor ? `<author>
-        <name>${defaultAuthor?.name}</name>
-    </author>` : ``
-    }${posts?.[0]?.modified?.date || posts?.[0]?.published?.date ? `<updated>${new Date(posts?.[0]?.modified?.date || posts?.[0]?.published?.date).toISOString()}</updated>` : ``}
-    <generator uri="https://press.lorecord.com" version="0.0.1">Press</generator>
-    <link href="${siteConfig.url}" rel="alternate" type="text/html"/>
-    <link href="${siteConfig.url}/feed/" rel="self" type="application/atom+xml"/>
-${supportedLocales.map((locale: any) => {
-        return `<link href="${siteConfig.url}/${locale}/feed/" rel="alternate" type="application/atom+xml" hreflang="${locale}" />`
-    })}
-    <rights>Copyright (c)</rights>
-${posts.map((post: any) => `
-    <entry>
-        <id isPermaLink="true">${siteConfig.url}${post.route}</id>
-        <title>${post.title}</title>
-        <link href="${siteConfig.url}${post.route}" />
-        ${post.published.date
-            ? `<published>${new Date(post.published.date).toISOString()}</published>`
-            : ``}
-        ${post.modified?.date
-            ? `<updated>${new Date(post.modified?.date).toISOString()}</updated>`
-            : ``}
-        ${post.audio?.[0]
-            ? `<link rel="enclosure" type="audio/mpeg"
-        href="${post.audio?.[0]}"/>`
-            : ``}
-            ${post.video?.[0]
-            ? `<link rel="enclosure" type="video"
-            href="${post.video?.[0]}"/>`
-            : ``}
-        ${post.author
-            ? post.author.map((author: any) => `<author>
-            <name>${author.name}</name>
-        </author>`)
-            : ``}
-        ${post.contributor
-            ? post.contributor.map((c: any) => `
-        <contributor>
-            <name>${c}</name>
-        </contributor>
-            `)
-            : ``}
-        <summary><![CDATA[${escapeHtml(post.summary?.html)}]]></summary>
-        <content type="html"><![CDATA[${escapeHtml(post.content?.html)}]]></content>
-        ${post.taxonomy?.category
-            ? post.taxonomy.category
-                .map((category: any) => `<category>${category}</category>`).join('\n')
-            : ''}
-        ${post.taxonomy?.tag
-            ? post.taxonomy.tag
-                .map((tag: any) => `<category>${tag}</category>`).join('\n\t\t') : ''}
-    </entry>`
-    )
-        .join('')}
-</feed>
-`)
